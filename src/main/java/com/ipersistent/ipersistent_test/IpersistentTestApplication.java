997102435cb2b9965ff329dad2dfd531c08b4332
KaiXuan-Y <1730052751@qq.com>
2023-09-13 15:18:03 +0800

    create the ipersistent

---
+++ src/main/java/com/ipersistent/ipersistent_test/IpersistentTestApplication.java
@@ -0,0 +1,13 @@
+package com.ipersistent.ipersistent_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpersistentTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpersistentTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipersistent/ipersistent_test/IpersistentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipersistent.ipersistent_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpersistentTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
